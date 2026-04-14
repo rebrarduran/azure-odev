a76e52c100b838feb81f157fa4e6eebf1ff3a441
rebrarduran <rumeysaebrar26@gmail.com>
2026-04-14 14:45:31 +0300

    Modify response message in index.js
    Updated the response message in the root route.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(`
     <h1>Rumeysa Ebrar Duran</h1>
-    <h2> 221002053 (iö) </h2>
+    <h2> 221002053 (iö) denmeee </h2>
     
   `);
 });
