b9c5d3e20ba4fbdb1ed43c5ff70551be413ae628
rebrarduran <rumeysaebrar26@gmail.com>
2026-04-15 09:01:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(`
     <body style="background-color:#cdc0f0; text-align:center;">
-    <h1 style: "color:white;" >Rumeysa Ebrar Duran - Bulut Bilişim ödev</h1>
+    <h1 style="color: white;" >Rumeysa Ebrar Duran - Bulut Bilişim ödev</h1>
     <h2> 221002053 (iö) </h2>
     
   `);
